b892e4fee8d3705cdba9ede20a9fb408da23ea27
jhonanderson.munoz <jmunoz@k12.com>
2021-05-04 22:51:39 -0500

    add new files for project configuration

---
+++ src/main/java/com/intraway/project/IntrawayBeApplication.java
@@ -0,0 +1,13 @@
+package com.intraway.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrawayBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrawayBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intraway/project/IntrawayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intraway.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrawayBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
